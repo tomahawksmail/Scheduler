1b9c2325d660b8fbf29cad7ae6978a49ba8852b5
tomahawksmail <tomahawksmail@gmail.com>
2023-11-09 11:08:01 -0800

    add requirements.txt

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ import paramiko
 import re
 import os
 from dotenv import load_dotenv
+import pymysql
 
-host = "USKO-ittest"
 load_dotenv()
 
+connection = pymysql.connect(host=os.environ.get('DBHOST'),
+                             user=os.environ.get('DBUSER'),
+                             password=os.environ.get('DBPASSWORD'),
+                             database=os.environ.get('DATABASE'))
 
 
+# env
+host = "USKO-ittest"
 reg_file = "\\\\shots11\\tools\\sendmetrics\\gpologoff.reg"
 bat_file = "C:\\WINDOWS\\System32\\GroupPolicy\\User\\Scripts\\Logoff\\"
 # get schedule tasks
@@ -30,15 +36,19 @@ client.connect(host, username=os.environ.get('USER'), password=os.environ.get('P
 # append QB system users to filter their
 blackusers.append(re.compile(r'QBDataServiceUser').match(r'dir /b C:\Users'))
 
+# copy GPO bat file
+client.exec_command(r"xcopy \\shots11\tools\sendmetrics\send_logoff.bat C:\WINDOWS\System32\GroupPolicy\User\Scripts\Logoff\ ")
+
 # get real local users from host
 def getWhiteUsers():
     _stdin, _stdout, _stderr = client.exec_command(r'dir /b C:\Users')
     userlist = [x for x in _stdout.read().decode().splitlines() if x not in blackusers]
-
+    print(userlist)
     return userlist
 
 # apply reg-file for each user
 def ApplyREGFile(getWhiteUsers):
+
     for user in getWhiteUsers():
         path = f"C:\\Users\\{user}\\NTUSER.DAT"
 
@@ -46,9 +56,23 @@ def ApplyREGFile(getWhiteUsers):
         client.exec_command(f'reg import {reg_file}')
         client.exec_command('reg unload HKU\\Usko')
 
+        SQLrequest = """SELECT * from metricsStatus"""
+        try:
+            connection.connect()
+        except Exception as E:
+            print(E)
+        else:
+            with connection.cursor() as cursor:
+                cursor.execute(SQLrequest)
+            result = cursor.fetchall()
+            print(result)
+            cursor.close()
+            connection.close()
+
+
 
 ApplyREGFile(getWhiteUsers)
-client.exec_command(r"xcopy \\shots11\tools\sendmetrics\send_logoff.bat C:\WINDOWS\System32\GroupPolicy\User\Scripts\Logoff\ ")
+
 
 
 
@@ -60,9 +84,15 @@ for c in command:
         print(E)
     else:
         if out == '':
-            print(f'schtasks /create /xml "\\\\shots11\\tools\\sendmetrics\\{c[-2]}.xml" /tn "\\UskoInc\\{c[-2]}"')
+            print(f'Create and enable schedule task {c[-2]}')
+            client.exec_command(f'schtasks /create /xml "\\\\shots11\\tools\\sendmetrics\\{c[-2]}.xml" /tn "\\UskoInc\\{c[-2]}"')
         else:
             print("Scheduled yet")
 
 
 client.close()
+
+
+# delete schedule tasks
+#schtasks /end /tn "\UskoInc\1"
+#schtasks /delete /tn "\UskoInc\1" /f
